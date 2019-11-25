1bbc9849b575f4e339be586233b9814cef34492d
Juozas Rastenis <jra@novicell.dk>
2019-11-25 11:01:06 +0100

    NCFE-64:  coppied energifyn setup

---
+++ packages/vue/spa-cms-frontend/mixins/GridEditorMixin.vue
@@ -0,0 +1,17 @@
+
+<script lang="ts">
+import Vue from 'vue';
+import { Prop } from 'nuxt-property-decorator';
+
+export default class extends Vue {
+  @Prop({
+    default: {},
+  })
+  viewModel!: any;
+
+  @Prop({
+    default: 12,
+  })
+  size!: number;
+}
+</script>
