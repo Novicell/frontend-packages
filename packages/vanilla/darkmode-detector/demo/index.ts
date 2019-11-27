4e35e89c87ac35478a5c9e0b152b421cf44e08b5
Juozas Rastenis <juozas.rastenis@gmail.com>
2019-11-27 22:29:40 +0100

    demo added for checker

--- packages/vanilla/darkmode-detector/demo/index.ts
+++ packages/vanilla/darkmode-detector/demo/index.ts
@@ -1,7 +1,10 @@
-import { DarkmodeDetector } from '../lib';
+import { DarkmodeDetector, LocalTimeChecker } from '../lib';
 
 const DMDetector = new DarkmodeDetector();
 
 DMDetector.currentMode$.subscribe(val => {
   console.log(val);
 });
+
+const { isDark } = new LocalTimeChecker({howOften: 2});
+isDark.subscribe((val: boolean) => console.log(val));
