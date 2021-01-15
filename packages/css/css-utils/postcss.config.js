ae77b3baa6fd64f963d0b47e752b342b4784ef6b
Simon Eldevig <sne@novicell.dk>
2021-01-15 09:02:45 +0100

    Transform CSS variables to CDN version

--- packages/css/css-utils/postcss.config.js
+++ packages/css/css-utils/postcss.config.js
@@ -2,9 +2,16 @@ module.exports = {
   plugins: process.env.NODE_ENV === 'compile' ?
   [
     require('postcss-each'),
-    require('postcss-custom-media')
+    require('postcss-custom-properties')({
+      importFrom: 'src/partials/_variables.css',
+      preserve: false
+    }),
+    require('postcss-calc'),
+    require('postcss-custom-media')({
+      importFrom: 'src/partials/_media-queries.css'
+    })
   ] :
   [
-    require('postcss-each'),
+    require('postcss-each')
   ]
 }
