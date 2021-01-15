00800715ee59aac4c9fa4c823d232b069417edc9
Magnus Bendix Borregaard <mbb@novicell.dk>
2021-01-15 11:18:47 +0100

    vue-property-decorator not working

--- packages/vue/breadcrumb/lib/breadcrumb.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = breadcrumb;
-
-function breadcrumb() {
-    // TODO
-}
---
+++ packages/vue/breadcrumb/src/breadcrumb.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1 class="hello">Hello</h1>
+</template>
+
+<script lang="ts">
+import { Prop, Vue } from 'nuxt-property-decorator';
+
+export default class NcBreadcrumb extends Vue {
+  @Prop({
+    default: [],
+  })
+  links!: any;
+
+  @Prop({
+    default: '',
+  })
+  modifier!: string;
+}
+</script>
+
+<style scoped>
+.hello {
+  color: red;
+}
+</style>
