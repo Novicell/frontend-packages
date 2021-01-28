719e6100cd52b6a650501bf8b7a4135314249a7b
Magnus Bendix Borregaard <mbb@novicell.dk>
2021-01-28 10:17:18 +0100

    postcss plugin allows for node-resolving style imports, not in <style>, components are still not effective, change css-package to output index.css

---
+++ packages/css/breadcrumb/postcss.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  plugins: process.env.NODE_ENV === 'compile' ?
+  [
+    require("stylelint"),
+    require('postcss-nested'),
+    // require('postcss-each'),
+    // require('postcss-custom-properties')({
+    //   preserve: false
+    // }),
+    // require('postcss-calc'),
+    // require('postcss-custom-media')({
+    //   importFrom: 'src/partials/_media-queries.css'
+    // }),
+    // require('postcss-percentage')
+  ] :
+  [
+    require("stylelint"),
+    // require('postcss-each'),
+    // require('postcss-percentage')
+  ]
+}
