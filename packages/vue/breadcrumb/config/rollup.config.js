89cc0eecc004fd3a5489a66932598c0d145db79b
Magnus Bendix Borregaard <mbb@novicell.dk>
2021-02-19 12:15:01 +0100

    refactor(vue-breadcrumb,vue-link): hoist rollup config

--- packages/vue/breadcrumb/config/rollup.config.js
+++ packages/vue/breadcrumb/config/rollup.config.js
@@ -1,10 +1,7 @@
-// Vue component Rollup config from Vue docs: https://vuejs.org/v2/cookbook/packaging-sfc-for-npm.html#How-do-I-configure-the-Rollup-build
-import commonjs from '@rollup/plugin-commonjs' // Convert CommonJS modules to ES6
-import vue from 'rollup-plugin-vue' // Handle .vue SFC files
-import buble from '@rollup/plugin-buble' // Transpile/polyfill with reasonable browser support
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import postcss from 'rollup-plugin-postcss'
-// import css from 'rollup-plugin-css-only' // Til separat css-bundle
+
+const { plugins } = require('../../config/rollup')
 
 export default {
     input: 'src/wrapper.js', // Path relative to package.json
@@ -12,18 +9,9 @@ export default {
         name: 'NcBreadcrumb',
         exports: 'named',
     },
-    plugins: [
+    plugins: [ // Order matters
         nodeResolve(),
-        postcss(),
-        commonjs(),
-        // css({ output: 'css/index.css' }), // Til separat css-bundle
-        vue({
-            css: true, // Dynamically inject css as a <style> tag
-            compileTemplate: true, // Explicitly convert template to render function
-            template: {
-                isProduction: true,
-            },
-        }),
-        buble(), // Transpile to ES5
-    ],
+        postcss(), //! Gives annoying linting warnings, but will most likely be removed soon
+        ...plugins,
+    ]
 };
---
+++ packages/vue/config/rollup.js
@@ -0,0 +1,19 @@
+const commonjs = require('@rollup/plugin-commonjs')
+const vue = require('rollup-plugin-vue')
+const buble = require('@rollup/plugin-buble')
+
+
+const plugins = [
+  commonjs(),
+  // css({ output: 'css/index.css' }), // Til separat css-bundle
+  vue({
+    css: true, // Dynamically inject css as a <style> tag
+    compileTemplate: true, // Explicitly convert template to render function
+    template: {
+      isProduction: true,
+    },
+  }),
+  buble(), // Transpile to ES5
+]
+
+module.exports.plugins = exports.plugins = plugins
