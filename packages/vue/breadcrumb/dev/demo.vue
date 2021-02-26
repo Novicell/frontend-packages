6598ec041d951e9ace5328c8b39f82b38f06d3a8
Magnus Bendix Borregaard <mbb@novicell.dk>
2021-02-26 11:14:00 +0100

    refactor: add a demo wrapper to vue components
    npm run dev now always runs dev/demo.vue that applies styling to component etc

---
+++ packages/vue/breadcrumb/dev/demo.vue
@@ -0,0 +1,14 @@
+<template>
+  <nc-breadcrumb />
+</template>
+
+<script>
+import '@novicell/styles-breadcrumb' // styles are not included by default
+import NcBreadcrumb from '../src/nc-breadcrumb'
+
+export default {
+  components: {
+    NcBreadcrumb
+  },
+};
+</script>
---
+++ packages/vue/link/dev/demo.vue
@@ -0,0 +1,13 @@
+<template>
+  <nc-link />
+</template>
+
+<script>
+import NcLink from '../src/nc-link'
+
+export default {
+  components: {
+    NcLink
+  },
+};
+</script>
