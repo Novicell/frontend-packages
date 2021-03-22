30d7aed83c7cf19b59a6b55b928fe4cf7d6215a3
Magnus Bendix Borregaard <mbb@novicell.dk>
2021-03-22 14:38:48 +0100

    feat(vue-carousel): defaults to design pattern image slideshow

--- packages/vue/carousel/dev/demo.vue
+++ packages/vue/carousel/dev/demo.vue
@@ -1,7 +1,10 @@
 <template>
 <div class="container">
   <nc-carousel>
-    <div v-for="i in 5" :key="i">{{ i }}</div>
+    <img class="item img" src="https://source.unsplash.com/random/1920x1080" alt="random splash">
+    <img class="item img" src="https://source.unsplash.com/random/800x600" alt="random splash">
+    <img class="item img" src="https://source.unsplash.com/random/1440x900" alt="random splash">
+    <div class="item funky"></div>
   </nc-carousel>
 </div>
 </template>
@@ -13,6 +16,25 @@ export default {
   components: {
     NcCarousel
   },
+
+  // data() {
+  //   return {
+  //     swiperOptions: {
+  //       spaceBetween: 30,
+  //       slidesPerView: 1,
+  //       grabCursor: true,
+  //       effect: 'fade',
+  //       autoplay: true,
+  //       pagination: {
+  //         clickable: true,
+  //         el: '.swiper-pagination'
+  //       },
+  //       scrollbar: {
+  //         el: '.swiper-scrollbar'
+  //       }
+  //     }
+  //   }
+  // }
 };
 </script>
 
@@ -21,7 +43,21 @@ export default {
   height: 100vh;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: stretch;
+}
+
+.item {
+  height: 100%;
+  width: 100%;
+}
+
+.img {
+  object-fit: cover;
+  object-position: center;
+}
+
+.funky {
+  background: hotpink;
 }
 
 
--- packages/vue/carousel/src/nc-carousel.vue
+++ packages/vue/carousel/src/nc-carousel.vue
@@ -1,15 +1,4 @@
-<template>
 
-
-  <Swiper ref="image-slider" class="carousel" :options="swiperOptions">
-    <SwiperSlide v-for="slot in $slots.default" :key="slot" class="carousel__slide">
-      <div class="item">Hello</div>
-    </SwiperSlide>
-
-    <div slot="pagination" class="swiper-pagination"></div>
-  </Swiper>
-
-</template>
 
 <script>
 import { Swiper, SwiperSlide } from 'vue-awesome-swiper';
@@ -31,37 +20,123 @@ SPECS:
 
 //! use: https://github.surmon.me/vue-awesome-swiper/?ref=madewithvuejs.com
 export default {
-  mounted() {
-    console.log(this.$slots.default)
-  },
   components: {
     Swiper,
     SwiperSlide
   },
 
-  data() {
-    return {
-      swiperOptions: {
-        spaceBetween: 0,
-        slidesPerView: 3,
-        grabCursor: true,
-        autoplay: true,
-        pagination: {
-          clickable: true,
-          el: '.swiper-pagination'
-        },
+  props: {
+    options: {
+      type: Object,
+      default() {
+        return {
+          spaceBetween: 30,
+          slidesPerView: 1,
+          grabCursor: true,
+          effect: 'fade',
+          autoplay: true,
+          pagination: {
+            clickable: true,
+            el: '.swiper-pagination'
+          },
+          // a11y: {
+          //   prevSlideMessage: this.$t('Accessibility.SlideShow.PrevSlideMessage'),
+          //   nextSlideMessage: this.$t('Accessibility.SlideShow.NextSlideMessage'),
+          //   firstSlideMessage: this.$t('Accessibility.SlideShow.FirstSlideMessage'),
+          //   lastSlideMessage: this.$t('Accessibility.SlideShow.LastSlideMessage'),
+          //   paginationBulletMessage: this.$t('Accessibility.SlideShow.PaginationBulletMessage')
+          // }
+        }
       }
     }
-  }
+  },
+
+  render(h) {
+    // For every element passed into default slot: wrap it in a SwiperSlide
+    const slides = this.$slots.default.map((node) => {
+      return h(
+        SwiperSlide,
+        {
+          key: node.key // move the passed key onto SwiperSlide (if using v-for)
+        },
+        [node] // must be an array although there is just one node
+      )
+    })
+
+    // These are only created if their options are specified in this.options
+    let pagination
+    if (this.options.pagination) {
+      pagination = h(
+        'div',
+        {
+          class: 'swiper-pagination',
+          slot: 'pagination'
+        }
+      )
+    }
+
+    let scrollbar
+    if (this.options.scrollbar) {
+      scrollbar = h(
+        'div',
+        {
+          class: 'swiper-scrollbar',
+          slot: 'scrollbar'
+        }
+      )
+    }
+
+    let navigationPrev
+    let navigationNext
+    if (this.options.navigation) {
+       navigationPrev = h(
+        'div',
+        {
+          class: 'swiper-button-prev',
+          slot: 'button-prev'
+        }
+      )
+
+      navigationNext = h(
+        'div',
+        {
+          class: 'swiper-button-next',
+          slot: 'button-next'
+        }
+      )
+    }
+
+
+    // If pagination or navigation are undefined, they will just be dropped by vue
+    const children = [...slides, pagination, scrollbar, navigationPrev, navigationNext ]
+
+    // Render a Swiper with the SwiperSlides + pagination slot as children
+    return h(
+      Swiper,
+      {
+        ref: 'image-slider',
+        class: 'carousel',
+        props: {
+          options: this.options // The Vue Awesome Swiper options passed from outside
+        }
+      },
+      children
+    )
+  },
+
 }
 
 </script>
 
 <style scoped>
-.item {
-  height: 500px;
-  width: 300px;
-  background: hotpink;
-  color: white;
+/*
+! move this to reusable styling
+*/
+
+.carousel {
+  --swiper-theme-color: white;
+
+  height: 50vh;
+  width: 100%;
 }
 </style>
